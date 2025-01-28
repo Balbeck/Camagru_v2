46d900a05ae4e82a0a4b8c567ca161087f090e44
HectorAlbecker <hector.albecker@sanofi.com>
2025-01-28 22:53:32 +0100

    add: config for mongo

--- backend/node_api/src/schemas/userSchema.ts
+++ backend/node_api/src/schemas/userSchema.ts
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+    description: String,
+    complete: Boolean
+});
+
+module.exports.Users = mongoose.model('User', userSchema);
