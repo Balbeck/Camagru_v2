e94cab3a4b90c6a49022e7812b0be67e7c3d36ec
HectorAlbecker <hector.albecker@sanofi.com>
2025-01-30 21:55:46 +0100

    clear: index.ts

--- backend/node_api/src/index.ts
+++ backend/node_api/src/index.ts
@@ -1,22 +1,18 @@
 // console.log('hello World 🌏')
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
-// import mongoose from 'mongoose';
 import connectMongoDb from './db_config/connectMongoDb';
-
-// import Interface for typescript
-import { IUser } from 'schemas/userSchema';
+import userRoutes from './routes/userRoutes';
 
 import dotenv from 'dotenv';
 dotenv.config();
 const port: number = parseInt(process.env.BACKEND_PORT);
 
-// [ Routes ]
-import userRoutes from './routes/userRoutes';
+
 
 // - - - [ Create App ] - - -
 const app: Express = express();
@@ -29,33 +25,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
 
+// - - - [ Routes ] - - -
 app.use('/user', userRoutes);
-
-
-
-
-// // - - - [ - R-o-u-t-e-s ] - - -
-// app.get('/test', (req: Request, res: Response) => {
-//     res.send('API REST avec Node.js, Express et MongoDB en TypeScript');
-
-// });
-
-// app.post('/test', (req: Request, res: Response) => {
-//     res.send({ message: 'API REST avec Node.js, Express et MongoDB en TypeScript' });
-// });
-
-// // Exemple de route asynchrone
-// app.get('/users', async (req: Request, res: Response) => {
-//     try {
-//         const users = await User<IUser>.find();
-//         res.json(users);
-//     } catch (err) {
-//         res.status(500).json({ message: 'Erreur lors de la récupération des utilisateurs', error: err });
-//     }
-// });
-
-
-
 
 
 
@@ -70,9 +41,3 @@ const startServer = async (): Promise<void> => {
 startServer().catch((err) => {
     console.error('Erreur lors du démarrage du serveur:', err);
 });
-
-// const server = http.createServer(app)
-// server.listen(port, () => {
-//     console.log(` 🐳 Back Server is running 🌏n http://localhost:${port}/ 🚀`)
-// })
-
