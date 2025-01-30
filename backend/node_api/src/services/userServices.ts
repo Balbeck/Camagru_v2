d544294675cfff70756a4e98c0c4e4ecbd44959e
Balbeck <albecker.hector@gmail.com>
2025-01-30 20:30:10 +0100

    unsolved: bcrypt

--- backend/node_api/src/services/userServices.ts
+++ backend/node_api/src/services/userServices.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 export const createUser = async (userData: Partial<IUser>): Promise<IUser> => {
     // Verif si le User existe deja
-    const existingUser = await User.findOne({ $or: [{email: String}, {username: String}] });
+    const existingUser = await User.findOne({ $or: [{ email: String }, { username: String }] });
     if (existingUser) {
         if (existingUser.email === userData.email) {
             throw new Error('EMAIL_ALREADY_EXISTS');
@@ -11,12 +11,16 @@ export const createUser = async (userData: Partial<IUser>): Promise<IUser> => {
             throw new Error('USERNAME_ALREADY_EXISTS')
         }
     }
+    console.log(` 🥝 User: ${userData} Doesnt already exist`)
     // Hash le password
-    const hashedPasword = await bcrypt.hash(userData.password, 10);
+    const pass: string = userData.password;
+    console.log(` 🚀 userData.password: ${userData.password}`)
+    const hashedPasword = await bcrypt.hash(pass, 10);
     // Creer le new object (Schema) User
     const newUser = new User({
         ...userData,
         password: hashedPasword,
     });
+    console.log(` 🦧 newUser to Create: ${newUser}`);
     return await newUser.save()
 };
