213f6ac9323ffa8584c43555129e40f42bd0896f
Balbeck <albecker.hector@gmail.com>
2025-01-30 21:52:31 +0100

    fix: ❌node:alpine, ✅ bcrypt, mongo -> verif + creation

--- backend/node_api/src/controllers/userControllers.ts
+++ backend/node_api/src/controllers/userControllers.ts
@@ -7,19 +7,19 @@ export const register = async (req: Request, res: Response) => {
 
         console.log('req.body: ', req.body);
         const newUser = await UserService.createUser(req.body);
-        const jwt = AuthJwt.generateJwt(newUser.id);
-        res.status(201).json({newUser, jwt});
+        console.log(` 🦄 newUser Created: ${newUser.username.toString()} ${newUser._id.toString()}`)
+        const jwt = AuthJwt.generateJwt(newUser._id.toString());
+        res.status(201).json({ newUser, jwt });
 
     } catch (error: any) {
         if (error.message === 'EMAIL_ALREADY_EXISTS') {
-            res.status(400).json({message: 'Email already in use'});
+            res.status(400).json({ message: 'Email already in use' });
         } else if (error.message === 'USERNAME_ALREADY_EXISTS') {
-            res.status(400).json({message: 'Username already in use'});
+            res.status(400).json({ message: 'Username already in use' });
         } else {
-            res.status(500).json({message: 'Internal Server Error'});
+            res.status(500).json({ message: 'Internal Server Error' });
         }
     }
-    UserService.createUser(req)
 };
 
 
--- backend/node_api/src/services/userServices.ts
+++ backend/node_api/src/services/userServices.ts
@@ -1,26 +1,34 @@
 import { User, IUser } from '../schemas/userSchema'
 import bcrypt from 'bcrypt';
 
-export const createUser = async (userData: Partial<IUser>): Promise<IUser> => {
-    // Verif si le User existe deja
-    const existingUser = await User.findOne({ $or: [{ email: String }, { username: String }] });
-    if (existingUser) {
-        if (existingUser.email === userData.email) {
-            throw new Error('EMAIL_ALREADY_EXISTS');
-        } else {
-            throw new Error('USERNAME_ALREADY_EXISTS')
+export const createUser = async (body: any): Promise<IUser> => {
+    try {
+        // Verif si le User existe deja
+        const existingUser = await User.findOne({ $or: [{ email: body.email }, { username: body.username }] });
+        if (existingUser) {
+            if (existingUser.email === body.email) {
+                throw new Error('EMAIL_ALREADY_EXISTS');
+            } else {
+                throw new Error('USERNAME_ALREADY_EXISTS')
+            }
         }
+        console.log(` 🥝 User: ${body} Doesnt already exist`)
+        // Hash le password
+        const pass: string = body.password;
+        console.log(` 🚀 userData.password: ${body.password}`)
+        const hashedPasword = await bcrypt.hash(pass, 10);
+        // const hashedPasword = await bcrypt.hash(userData.password, 10);
+        // Creer le new object (Schema) User
+        const newUser = new User({
+            ...body,
+            password: hashedPasword,
+        });
+        console.log(` 🦧 newUser to Create: ${newUser}`);
+        const savedUser = await newUser.save();
+        console.log("✅ User cree avec succes :", savedUser);
+        return savedUser;
+    } catch (err: any) {
+        console.error("❌ Error User Creation :", err.message);
+        throw err;
     }
-    console.log(` 🥝 User: ${userData} Doesnt already exist`)
-    // Hash le password
-    const pass: string = userData.password;
-    console.log(` 🚀 userData.password: ${userData.password}`)
-    const hashedPasword = await bcrypt.hash(pass, 10);
-    // Creer le new object (Schema) User
-    const newUser = new User({
-        ...userData,
-        password: hashedPasword,
-    });
-    console.log(` 🦧 newUser to Create: ${newUser}`);
-    return await newUser.save()
 };
