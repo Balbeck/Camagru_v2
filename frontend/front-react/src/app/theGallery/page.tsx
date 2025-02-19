9bcb70be1ef220a1e840f67f4fdaa0666dd655e0
Balbeck <albecker.hector@gmail.com>
2025-02-19 19:45:45 +0100

    feat: TheGalerie + pics

--- frontend/front-react/src/app/theGallery/page.tsx
+++ frontend/front-react/src/app/theGallery/page.tsx
@@ -16,23 +16,51 @@ const GalleryPage: React.FC = () => {
         {
             id: 1,
             title: 'Mon premier post',
-            photoUrl: 'https://via.placeholder.com/640x480',
+            photoUrl: '/pacific_1.jpeg',
             likes: 10,
             lastComment: 'Super photo !',
         },
         {
             id: 2,
             title: 'Un autre post',
-            photoUrl: 'https://via.placeholder.com/640x480',
+            photoUrl: '/pacific_2.jpeg',
             likes: 5,
             lastComment: 'Joli !',
+        },
+        {
+            id: 3,
+            title: 'Un autre post',
+            photoUrl: '/pacific_3.jpeg',
+            likes: 5,
+            lastComment: 'Quel Paysage !',
+        },
+        {
+            id: 4,
+            title: 'Un autre post',
+            photoUrl: '/pacific_4.jpeg',
+            likes: 7,
+            lastComment: 'Quel Paysage Magnifique!',
+        },
+        {
+            id: 5,
+            title: 'Un autre post',
+            photoUrl: '/pacific_5.jpeg',
+            likes: 17,
+            lastComment: 'Ouf ! 🌟 ',
+        },
+        {
+            id: 6,
+            title: 'Un autre post',
+            photoUrl: '/pacific_6.jpeg',
+            likes: 7,
+            lastComment: 'Bonne Heure ! 🌞 ',
         },
         // Ajoute d'autres posts ici
     ];
 
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Galerie</h1>
+            <h1 className="text-2xl font-bold mb-4">The Galerie</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {posts.map((post) => (
                     <PostComponent
